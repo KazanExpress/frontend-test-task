838ca089c18fb1f16f45baa18d9a67ef8286dbf5
lookrecius <bragin.boris@ya.ru>
2020-04-05 04:21:28 +0300

    Added imports format by tslint

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,6 @@ import store from './store';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import 'material-design-icons/iconfont/material-icons.css';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 
 Vue.config.productionTip = false;
 
