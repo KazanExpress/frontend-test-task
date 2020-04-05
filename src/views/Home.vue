78719e02134a8c177f13bc30c88922c9ed67ae61
lookrecius <bragin.boris@ya.ru>
2020-04-05 04:17:37 +0300

    Added Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,115 @@
 <template>
-  <div class="home">
-
-
+  <div class="text-center align-items-center">
+    <div class="mb-4 row">
+      <div class="col-6 col-sm-12 col-md-6 text-left">
+        <div class="mb-3">
+          <h1
+                  v-if="!isChangeName"
+                  @click="isChangeName = !isChangeName"
+                  class="cursor-pointer m-0 d-inline-block"
+          >{{name}}</h1>
+          <add-description
+                  v-else
+                  :text="name"
+                  @updateText="updateName"
+                  @customAction="toggleVisible"
+          ></add-description>
+        </div>
+        <export-j-s-o-n
+                :filename="name"
+                :format-file="formatFileForExport"
+                :data="dataForExport"
+        ></export-j-s-o-n>
+      </div>
+      <div class="col-6 col-sm-12 col-md-6 text-left">
+        <add-task @addTask="addTask"></add-task>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <task-items
+              @destroyTask="destroyTask"
+              :taskList="taskList"
+              class="col col-md-12"
+      ></task-items>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import Task from '@/components/Task.vue';
+<script lang="ts">
 
-export default {
-  name: 'Task',
-  components: {
-    Task,
-  },
-};
+  import { Component, Emit, Vue } from 'vue-property-decorator';
+  import store from '@/store';
+  import { namespace } from 'vuex-class';
+  import AddDescription from '@/components/AddDescription.vue';
+  import TaskItems from '@/components/TaskItems.vue';
+  import AddTask from '@/components/AddTask.vue';
+  import ExportJSON from '@/components/ExportJSON.vue';
+  import { ITaskItems } from '@/interfaces/IApplication';
+
+  const tasker = namespace('tasker');
+
+  Component.registerHooks([
+    'beforeRouteEnter'
+  ]);
+
+  @Component({
+    components: {
+      AddDescription,
+      TaskItems,
+      AddTask,
+      ExportJSON,
+    },
+  })
+  export default class Home extends Vue {
+    @tasker.State
+    public name!: string;
+
+    @tasker.State
+    private taskList!: ITaskItems[];
+
+    private isChangeName = false;
+    private formatFileForExport = 'json';
+
+    get dataForExport () {
+      return JSON.stringify({
+        taskList: this.taskList,
+      });
+    }
+
+    @Emit('updateTaskDescription')
+    public updateName (text: string): void {
+      this.$store.commit('tasker/updateName', text);
+    }
+
+    @Emit('saveEmitter')
+    public toggleVisible (): void {
+      this.isChangeName = false;
+    }
+
+    @Emit('addTask')
+    public addTask (task: ITaskItems): void {
+      this.taskList.unshift(task);
+    }
+
+    @Emit('destroyTask')
+    public destroyTask ({ index, taskList }: { index: number; taskList: ITaskItems[] }): void {
+      taskList.splice(index, 1);
+    }
+
+    public async beforeRouteEnter (to: any, from: any, next: any) {
+      const taskerName = to.query.name;
+      if (typeof taskerName === 'undefined') {
+        next('/Home');
+      }
+      await store.dispatch('tasker/search', { name: taskerName });
+      if (store.state.tasker.name !== taskerName) {
+        next('/Home');
+      }
+      next();
+    }
+  }
 </script>
+
+<style scoped>
+
+</style>
