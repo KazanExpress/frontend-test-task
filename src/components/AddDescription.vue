5b4af0ce9ffa1ea17eff77088acc5de8bbd3e4a0
lookrecius <bragin.boris@ya.ru>
2020-04-05 04:22:16 +0300

    Add AddDesription component

--- src/components/AddDescription.vue
+++ src/components/AddDescription.vue
@@ -1,11 +1,29 @@
 <template>
-    $END$
+    <div class="input-group input-group-lg col-12 p-0">
+        <input v-model="descriptionText" type="text" class="form-control">
+        <div class="input-group-append">
+            <button type="button" class="btn btn-success input-group-text" @click="$emit('saveEmitter')">Save</button>
+        </div>
+    </div>
 </template>
 
-<script>
-    export default {
-        name: "AddDescription"
+<script lang="ts">
+
+  import { Component, Prop, Vue } from 'vue-property-decorator';
+
+  @Component
+  export default class AddDescription extends Vue {
+    @Prop({ type: String, required: true })
+    private text!: string;
+
+    get descriptionText () {
+      return this.text;
     }
+
+    set descriptionText (value) {
+      this.$emit('updateTaskDescription', value);
+    }
+  }
 </script>
 
 <style scoped>
