7c8591a20276cb30cbb6e25ea06e11818acc03c2
lookrecius <bragin.boris@ya.ru>
2020-04-05 19:10:43 +0300

    Fix emitter and add task

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -1,27 +1,17 @@
 <template>
-<!--    <form >-->
-<!--        <div class="form-group">-->
-<!--            <label for="task_name">Task name</label>-->
-<!--            <input v-model="task.name" type="text" class="form-control" id="task_name" placeholder="Please enter task name" required>-->
-<!--        </div>-->
-<!--        <div class="form-group">-->
-<!--            <label for="task_description">Description</label>-->
-<!--            <input v-model="task.description" type="text" class="form-control" id="task_description" placeholder="Please enter description for task" required>-->
-<!--        </div>-->
-<!--    </form>-->
 
     <div class="row">
         <form class="col s12" @submit.prevent="addItemTaskHandler">
             <div class="row">
                 <div class="input-field col s6">
-                    <i class="material-icons prefix">account_circle</i>
-                    <input  v-model="task.name" id="icon_prefix" type="text" class="validate">
-                    <label for="icon_prefix">Имя</label>
+                    <i class="material-icons prefix">note_add</i>
+                    <input  v-model="item.name" id="icon_prefix" type="text" class="validate" required>
+                   <p>Имя задачи</p>   <label for="icon_prefix"></label>
                 </div>
                 <div class="input-field col s6">
-                    <i class="material-icons prefix">phone</i>
-                    <input  v-model="task.description" id="icon_telephone" type="tel" class="validate">
-                    <label for="icon_telephone">Описание</label>
+                    <i class="material-icons prefix">mode_edit</i>
+                    <input  v-model="item.description" id="icon_telephone" type="tel" class="validate" required>
+                    <p>Описание</p>  <label for="icon_telephone"></label>
                 </div>
             </div>
             <button type="submit" class="btn waves-effect waves-light"><i class="material-icons right">add</i>Добавить</button>
@@ -31,18 +21,15 @@
 
 </template>
 
+
 <script lang="ts">
-  /**
-   * This component is designed to create tasks.
-   * @emits { Object } addTask
-   * */
 
   import { Component, Vue } from 'vue-property-decorator';
   import { ITaskItems } from '@/interfaces/IApplication';
 
   @Component
   export default class AddTask extends Vue {
-    private task: ITaskItems = {
+    private item: ITaskItems = {
       name: '',
       description: '',
       isChecked: false,
@@ -50,10 +37,10 @@
     };
 
     private addItemTaskHandler () {
-      const newItem = JSON.parse(JSON.stringify(this.task));
-      this.$emit('addItemTaskHandler', newItem);
-      this.task.name = '';
-      this.task.description = '';
+      const newItem = JSON.parse(JSON.stringify(this.item));
+      this.$emit('addTask', newItem);
+      this.item.name = '';
+      this.item.description = '';
     }
   }
 </script>
