44ec953c9427ed7489be8c7e9b93daaf42df0500
lookrecius <bragin.boris@ya.ru>
2020-04-05 19:09:48 +0300

    Fix path and name and const routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,12 +11,12 @@ const routes = [
     component: () => import('@/views/TaskApp.vue'),
   },
   {
-    path: '/apptaskhandler',
-    name: 'AppTaskHandler',
+    path: '/tasker',
+    name: 'Tasker',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '@/views/AppTaskHandler.vue'),
+    component: () => import(/* webpackChunkName: "about" */ '@/views/Tasker.vue'),
   }
 ];
 
